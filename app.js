c70da2fe39e79c59458ff1da4a00151190229988
beata <beata@emc2wave.com>
2020-01-28 21:02:21 -0800

    add second page catch err

--- app.js
+++ app.js
@@ -10,6 +10,22 @@
 //   });
 
 // ******* Axios chain request ******
-axios.get("https://swapi.co/api/planets/").then(({ data }) => {
-  console.log(data);
-});
+axios
+  .get("https://swapi.co/api/planets/")
+  .then(({ data }) => {
+    console.log(data);
+    console.log("FIRST PAGE");
+    for (let planet of data.results) {
+      console.log(planet.name);
+    }
+    return axios.get(data.next);
+  })
+  .then(({ data }) => {
+    console.log("SECOND PAGE");
+    for (let planet of data.results) {
+      console.log(planet.name);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  });
