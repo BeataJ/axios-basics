f40b6067c1b60733128b043d86310810a11e4aa3
beata <beata@emc2wave.com>
2020-01-28 20:51:51 -0800

    Axios chain request

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-axios
-  .get("https://swapi.co/api/planets/")
-  .then(res => {
-    console.log(res.data);
-  })
-  .catch(err => {
-    console.log("IN CATCH");
-    console.log(err);
-  });
+// **** Axios basics ***
+// axios
+//   .get("https://swapi.co/api/planets/")
+//   .then(res => {
+//     console.log(res.data);
+//   })
+//   .catch(err => {
+//     console.log("IN CATCH");
+//     console.log(err);
+//   });
+
+// ******* Axios chain request ******
+axios.get("https://swapi.co/api/planets/").then(({ data }) => {
+  console.log(data);
+});
