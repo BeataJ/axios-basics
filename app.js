2906e059cd1f835f630dc46b66acd82493de3da3
beata <beata@emc2wave.com>
2020-01-28 22:05:39 -0800

    add  fetchNextPlanets() printPlanets

--- app.js
+++ app.js
@@ -10,22 +10,26 @@
 //   });
 
 // ******* Axios chain request ******
-axios
-  .get("https://swapi.co/api/planets/")
-  .then(({ data }) => {
-    console.log(data);
-    console.log("FIRST PAGE");
-    for (let planet of data.results) {
-      console.log(planet.name);
-    }
-    return axios.get(data.next);
-  })
-  .then(({ data }) => {
-    console.log("SECOND PAGE");
-    for (let planet of data.results) {
-      console.log(planet.name);
-    }
-  })
+
+const fetchNextPlanets = (url = "https://swapi.co/api/planets/") => {
+  return axios.get(url);
+};
+
+const printPlanets = ({ data }) => {
+  console.log(data);
+  console.log("**********");
+  for (let planet of data.results) {
+    console.log(planet.name);
+  }
+  return Promise.resolve(data.next);
+};
+
+fetchNextPlanets()
+  .then(printPlanets)
+  .then(fetchNextPlanets)
+  .then(printPlanets)
+  .then(fetchNextPlanets)
+  .then(printPlanets)
   .catch(err => {
     console.log(err);
   });
