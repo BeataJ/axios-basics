cd1853ed76172842f6625bec0f3f71ce4909d371
beata <beata@emc2wave.com>
2020-01-28 17:44:06 -0800

    check error how

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 axios
-  .get("https://swapi.co/api/planets/")
+  .get("https://swapi.co/api/planets500/")
   .then(res => {
     console.log(res.data);
   })
   .catch(err => {
+    console.log("IN CATCH");
     console.log(err);
   });
